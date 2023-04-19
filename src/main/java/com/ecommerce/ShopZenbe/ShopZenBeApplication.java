a275d13372c77d22d15c305d8837efec3e0d55d0
amanRathod <aksrathod07@gmail.com>
2023-04-19 09:34:54 +0530

    project setup

---
+++ src/main/java/com/ecommerce/ShopZenbe/ShopZenBeApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ShopZenbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopZenBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopZenBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/ShopZenbe/ShopZenBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ShopZenbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopZenBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
