51b0a36edf074bccb9582f551608cbfea488e5e5
amanRathod <aksrathod07@gmail.com>
2023-04-20 10:27:42 +0530

    create rest config

--- src/main/java/com/ecommerce/ShopZenbe/dao/ProductCategoryRepository.java
+++ src/main/java/com/ecommerce/ShopZenbe/dao/ProductCategoryRepository.java
@@ -2,8 +2,12 @@ package com.ecommerce.ShopZenbe.dao;
 
 import com.ecommerce.ShopZenbe.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.UUID;
 
+@CrossOrigin("http://localhost:3000")
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, UUID> {
 }
--- src/main/java/com/ecommerce/ShopZenbe/dao/ProductRepository.java
+++ src/main/java/com/ecommerce/ShopZenbe/dao/ProductRepository.java
@@ -2,8 +2,10 @@ package com.ecommerce.ShopZenbe.dao;
 
 import com.ecommerce.ShopZenbe.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.UUID;
 
+@CrossOrigin("http://localhost:3000")
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 }
