acb96ed17511727d77a3fdd9e55197afe64b7e64
amanRathod <aksrathod07@gmail.com>
2023-04-21 11:59:47 +0530

    create customer,address dto

---
+++ src/main/java/com/ecommerce/ShopZenbe/models/customer/CustomerDTO.java
@@ -0,0 +1,23 @@
+package com.ecommerce.ShopZenbe.models.customer;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerDTO {
+    @NotBlank(message = "First name is mandatory")
+    private String firstName;
+
+    @NotBlank(message = "Last name is mandatory")
+    private String lastName;
+
+    @Email(message = "Invalid email address")
+    private String email;
+}
