18b9b3c77850b74a8e76c9a7a730588c7dc17624
amanRathod <aksrathod07@gmail.com>
2023-05-20 08:39:01 +0530

    update cors origin url

--- src/main/java/com/ecommerce/ShopZenbe/config/MyAppConfig.java
+++ src/main/java/com/ecommerce/ShopZenbe/config/MyAppConfig.java
@@ -23,7 +23,7 @@ public class MyAppConfig  implements WebMvcConfigurer {
 //    @CrossOrigin(origins = "http://localhost:3000")
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://shop-zen-crm.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
