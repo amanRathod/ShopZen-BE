9649cbb7c841075b921d9f9f69ac7e9475888a57
Aman Kumar Singh <aksrathod07@gmail.com>
2023-05-21 18:32:53 +0530

    Encrypt secrets (#10)
    * create secrets.properties file
    
    * remove controller test
    
    * remove test file
    
    * create env file
    
    * update origin url
    
    * minor improve

--- src/main/java/com/ecommerce/ShopZenbe/ShopZenBeApplication.java
+++ src/main/java/com/ecommerce/ShopZenbe/ShopZenBeApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShopZenBeApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("spring.datasource.url", dotenv.get("DATABASE_URL"));
+//		Dotenv dotenv = Dotenv.load();
+//		System.setProperty("spring.datasource.url", dotenv.get("DATABASE_URL"));
 		SpringApplication.run(ShopZenBeApplication.class, args);
 	}
 
